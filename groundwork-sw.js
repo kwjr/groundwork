d07e6368fdf2a5cd58513408f1c163b91ebfc70e
Kenneth Wilson <ken.wilson@outlook.com>
2026-06-03 12:57:08 -0400

    Add files via upload

--- groundwork-sw.js
+++ groundwork-sw.js
@@ -1,6 +1,6 @@
 const CACHE = 'groundwork-v1';
 const ASSETS = [
-  './groundwork.html',
+  './index.html',
   'https://fonts.googleapis.com/css2?family=DM+Sans:opsz,wght@9..40,300;9..40,400;9..40,500;9..40,600;9..40,700&display=swap'
 ];
 
